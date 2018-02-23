40fe21eeed7a810b9ffeca80103a13e99ec35933
Neil Seejoor <nvseejoor@gmail.com>
2018-02-23 17:18:05 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,14 +6,10 @@ class Artist
     @name = name
     @songs = []
   end
-  #
-  # def songs=(song)
-  #   @songs = song
-  # end
-  #
-  # def songs
-  #   @songs
-  # end
+
+  def songs
+    @songs
+  end
 
   def add_song(song)
     @songs << song
