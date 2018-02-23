291ac53ee79a3529189509853814afe01f9b8afa
Neil Seejoor <nvseejoor@gmail.com>
2018-02-23 17:09:31 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,19 +1,19 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
 
   @songs = []
   def initialize(name)
     @name = name
     @songs = []
   end
-
-  def songs=(song)
-    @songs = song
-  end
-
-  def songs
-    @songs
-  end
+  # 
+  # def songs=(song)
+  #   @songs = song
+  # end
+  # 
+  # def songs
+  #   @songs
+  # end
 
   def add_song(song)
     @songs << song
